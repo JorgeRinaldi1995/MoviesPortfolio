8501dcf026aee3efddba7d4d83e77fb5c875b512
JorgeRinadli1995 <jorge.rinaldi.1995@gmail.com>
2022-09-21 20:09:19 -0300

    sass folder structure

--- web/themes/custom/movies_portfolio/gulpfile.js
+++ web/themes/custom/movies_portfolio/gulpfile.js
@@ -1,91 +1,80 @@
-let gulp = require('gulp'),
-  sass = require('gulp-sass')(require('sass')),
-  sourcemaps = require('gulp-sourcemaps'),
-  $ = require('gulp-load-plugins')(),
-  cleanCss = require('gulp-clean-css'),
-  rename = require('gulp-rename'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
-  postcssInlineSvg = require('postcss-inline-svg'),
-  browserSync = require('browser-sync').create()
-  pxtorem = require('postcss-pxtorem'),
-	postcssProcessors = [
-		postcssInlineSvg({
-      removeFill: true,
-      paths: ['./node_modules/bootstrap-icons/icons']
-    }),
-		pxtorem({
-			propList: ['font', 'font-size', 'line-height', 'letter-spacing', '*margin*', '*padding*'],
-			mediaQuery: true
-		})
-  ];
+const gulp = require('gulp')
+const gulpif = require('gulp-if')
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps')
+const cssnano = require('cssnano')
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const postcssInlineSvg = require('postcss-inline-svg')
+const browserSync = require('browser-sync').create()
+const pxtorem = require('postcss-pxtorem')
 
 const paths = {
   scss: {
-    src: './scss/style.scss',
+    entry: './scss/index.scss',
     dest: './css',
     watch: './scss/**/*.scss',
-    bootstrap: './node_modules/bootstrap/scss/bootstrap.scss',
   },
   js: {
     bootstrap: './node_modules/bootstrap/dist/js/bootstrap.min.js',
     popper: './node_modules/@popperjs/core/dist/umd/popper.min.js',
-    barrio: '../../contrib/bootstrap_barrio/js/barrio.js',
     dest: './js'
   }
 }
 
-// Compile sass into CSS & auto-inject into browsers
-function styles () {
-  return gulp.src([paths.scss.bootstrap, paths.scss.src])
+function isDevEnv() {
+  return process.env.NODE_ENV === 'development'
+}
+
+function styles(done) {
+  const plugins = [
+    postcssInlineSvg({
+      removeFill: true,
+      paths: ['./node_modules/bootstrap-icons/icons']
+    }),
+    pxtorem({
+			propList: ['*', '!letter-spacing'],
+      selectorBlackList: [/^html$/],
+      exclude: /node_modules/i
+		}),
+    autoprefixer(),
+    cssnano({
+      preset: ['default', {
+        discardComments: { removeAll: true }
+      }]
+    })
+  ]
+
+  gulp.src([paths.scss.entry])
     .pipe(sourcemaps.init())
-    .pipe(sass({
-      includePaths: [
-        './node_modules/bootstrap/scss',
-        '../../contrib/bootstrap_barrio/scss'
-      ]
-    }).on('error', sass.logError))
-    .pipe($.postcss(postcssProcessors))
-    .pipe(postcss([autoprefixer({
-      browsers: [
-        'Chrome >= 35',
-        'Firefox >= 38',
-        'Edge >= 12',
-        'Explorer >= 10',
-        'iOS >= 8',
-        'Safari >= 8',
-        'Android 2.3',
-        'Android >= 4',
-        'Opera >= 12']
-    })]))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.scss.dest))
-    .pipe(cleanCss())
-    .pipe(rename({ suffix: '.min' }))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(postcss(plugins))
+    .pipe(gulpif(isDevEnv, sourcemaps.write()))
     .pipe(gulp.dest(paths.scss.dest))
     .pipe(browserSync.stream())
+
+    done()
 }
 
 // Move the javascript files into our js folder
 function js () {
-  return gulp.src([paths.js.bootstrap, paths.js.popper, paths.js.barrio])
+  return gulp.src([paths.js.bootstrap, paths.js.popper])
     .pipe(gulp.dest(paths.js.dest))
     .pipe(browserSync.stream())
 }
 
-// Static Server + watching scss/html files
-function serve () {
+function serve() {
   browserSync.init({
     proxy: 'https://movies-portfolio.lndo.site/',
   })
 
-  gulp.watch([paths.scss.watch, paths.scss.bootstrap], styles).on('change', browserSync.reload)
+  gulp.watch([paths.scss.watch], styles).on('change', browserSync.reload)
 }
 
-const build = gulp.series(styles, gulp.parallel(js, serve))
+// const build = gulp.series(styles, js)
+const watch = gulp.series(styles, gulp.parallel(js, serve))
 
 exports.styles = styles
 exports.js = js
 exports.serve = serve
-
-exports.default = build
+exports.default = serve
