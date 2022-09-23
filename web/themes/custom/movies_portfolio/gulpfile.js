b37d7f35dbeb66940673024e2b7b140f24c85a65
JorgeRinadli1995 <jorge.rinaldi.1995@gmail.com>
2022-09-22 22:26:31 -0300

    layout rework

--- web/themes/custom/movies_portfolio/gulpfile.js
+++ web/themes/custom/movies_portfolio/gulpfile.js
@@ -1,80 +1,77 @@
-const gulp = require('gulp')
-const gulpif = require('gulp-if')
-const sass = require('gulp-sass')(require('sass'));
-const sourcemaps = require('gulp-sourcemaps')
-const cssnano = require('cssnano')
-const postcss = require('gulp-postcss')
-const autoprefixer = require('autoprefixer')
-const postcssInlineSvg = require('postcss-inline-svg')
-const browserSync = require('browser-sync').create()
-const pxtorem = require('postcss-pxtorem')
-
-const paths = {
-  scss: {
-    entry: './scss/index.scss',
-    dest: './css',
-    watch: './scss/**/*.scss',
-  },
-  js: {
-    bootstrap: './node_modules/bootstrap/dist/js/bootstrap.min.js',
-    popper: './node_modules/@popperjs/core/dist/umd/popper.min.js',
-    dest: './js'
-  }
-}
-
-function isDevEnv() {
-  return process.env.NODE_ENV === 'development'
-}
-
-function styles(done) {
-  const plugins = [
+let gulp = require('gulp'),
+  sass = require('gulp-sass')(require('sass')),
+  sourcemaps = require('gulp-sourcemaps'),
+  $ = require('gulp-load-plugins')(),
+  cleanCss = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  postcssInlineSvg = require('postcss-inline-svg'),
+  browserSync = require('browser-sync').create()
+pxtorem = require('postcss-pxtorem'),
+  postcssProcessors = [
     postcssInlineSvg({
       removeFill: true,
       paths: ['./node_modules/bootstrap-icons/icons']
     }),
     pxtorem({
-			propList: ['*', '!letter-spacing'],
-      selectorBlackList: [/^html$/],
-      exclude: /node_modules/i
-		}),
-    autoprefixer(),
-    cssnano({
-      preset: ['default', {
-        discardComments: { removeAll: true }
-      }]
+      propList: ['font', 'font-size', 'line-height', 'letter-spacing', '*margin*', '*padding*'],
+      mediaQuery: true
     })
-  ]
+  ];
 
-  gulp.src([paths.scss.entry])
+const paths = {
+  scss: {
+    src: './scss/index.scss',
+    dest: './css',
+    watch: './scss/**/*.scss',
+    bootstrap: './node_modules/bootstrap/scss/bootstrap.scss',
+  },
+  js: {
+    bootstrap: './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    popper: './node_modules/@popperjs/core/dist/umd/popper.min.js',
+    barrio: '../../contrib/bootstrap_barrio/js/barrio.js',
+    dest: './js'
+  }
+}
+
+// Compile sass into CSS & auto-inject into browsers
+function styles() {
+  return gulp.src([paths.scss.bootstrap, paths.scss.src])
     .pipe(sourcemaps.init())
-      .pipe(sass().on('error', sass.logError))
-      .pipe(postcss(plugins))
-    .pipe(gulpif(isDevEnv, sourcemaps.write()))
+    .pipe(sass({
+      includePaths: [
+        './node_modules/bootstrap/scss',
+        '../../contrib/bootstrap_barrio/scss'
+      ]
+    }).on('error', sass.logError))
+    .pipe($.postcss(postcssProcessors))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(cleanCss())
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest(paths.scss.dest))
     .pipe(browserSync.stream())
-
-    done()
 }
 
 // Move the javascript files into our js folder
-function js () {
-  return gulp.src([paths.js.bootstrap, paths.js.popper])
+function js() {
+  return gulp.src([paths.js.bootstrap, paths.js.popper, paths.js.barrio])
     .pipe(gulp.dest(paths.js.dest))
     .pipe(browserSync.stream())
 }
 
+// Static Server + watching scss/html files
 function serve() {
   browserSync.init({
     proxy: 'https://movies-portfolio.lndo.site/',
   })
 
-  gulp.watch([paths.scss.watch], styles).on('change', browserSync.reload)
+  gulp.watch([paths.scss.watch, paths.scss.bootstrap], styles).on('change', browserSync.reload)
 }
 
-// const build = gulp.series(styles, js)
-const watch = gulp.series(styles, gulp.parallel(js, serve))
+const build = gulp.series(styles, gulp.parallel(js, serve))
 
 exports.styles = styles
 exports.js = js
 exports.serve = serve
-exports.default = serve
+
+exports.default = build
